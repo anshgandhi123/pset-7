389918e2ff797835f4dd4904990ad3465c561720
Guest1 <my@email.comgit config --globak username my>
2020-01-20 09:57:42 -0500

    fixed exercise 10

--- src/exercises.js
+++ src/exercises.js
@@ -87,7 +87,7 @@ function middle(values) {
     array.push(thirdMiddle);
     return array;
   }
-}
+};
 
 function increasing(numbers) {
     if (numbers == undefined || numbers.some(isNaN) || Number.isInteger(numbers) || numbers.length < 3) {
@@ -95,17 +95,17 @@ function increasing(numbers) {
     }
     else {
       for (let i = 1; i < numbers.length; i++) {
-       if ((numbers[i - 1] + 1) === numbers[i]) {
-         i++
-         if ((numbers[i - 1] + 1) === numbers[i]) {
-           return true;
-           break;
-         }
-       }
+        if ((numbers[i - 1] + 1) === numbers[i]) {
+          i++
+            if ((numbers[i - 1] + 1) === numbers[i]) {
+              return true;
+              break;
+            }
+        }
       }
-     }
-     return false;
-   }
+    }
+    return false;
+  };
 
 function everywhere(values, x) {
   if (values == undefined || values < 1 || x == undefined) {
@@ -115,99 +115,101 @@ function everywhere(values, x) {
     return false;
   }
   else {
-        for (let check = values.indexOf(x); check < values.length; check++) {
-          if (values[check] === values[check + 1] && values[check + 1] !== undefined) {
-            return true;
-            break;
-          } else if (x === 0) {
-            return false;
-            break;
-          } else if (values[check] === values[check + 2] && values[check + 2] !== undefined && values[check + 1] !== values[check]) {
-            return true;
-            break;
-          } else if (values[check] === values[check + 3] && values[check + 3] !== undefined && (values[check + 1] && values[check + 2]) !== values[check + 3]) {
-            return true;
-            break;
-          } else {
-            return false;
-            break;
-          }
-        }
+    for (let check = values.indexOf(x); check < values.length; check++) {
+      if (values[check] === values[check + 1] && values[check + 1] !== undefined) {
+        return true;
+        break;
+      }
+      else if (x === 0) {
+        return false;
+        break;
+      }
+      else if (values[check] === values[check + 2] && values[check + 2] !== undefined && values[check + 1] !== values[check]) {
+        return true;
+        break;
+      }
+      else if (values[check] === values[check + 3] && values[check + 3] !== undefined && (values[check + 1] && values[check + 2]) !== values[check + 3]) {
+        return true;
+        break;
+      }
+      else {
+        return false;
+        break;
+      }
     }
   }
+}
 
 function consecutive(numbers) {
   let value = 0;
-   if (numbers == undefined || numbers.length < 3 || numbers.some(isNaN) || numbers.some(Number.isInteger) === false) {
-     return false;
-   }
-   else {
-     for (let i = 0; i < numbers.length - 1; i++) {
-       if (numbers[i] % 2 === 0 && numbers[i + 1] % 2 === 0 && numbers[i + 2] % 2 === 0) {
-         value = 1;
-         break;
-       }
-       else if (numbers[i] % 2 !== 0 && numbers[i + 1] % 2 !== 0 && numbers[i + 2] % 2 !== 0) {
-         value = 1;
-         break;
-       }
-     }
-     if (value === 1) {
+  if (numbers == undefined || numbers.length < 3 || numbers.some(isNaN) || numbers.some(Number.isInteger) === false) {
+    return false;
+  };
+  else {
+    for (let i = 0; i < numbers.length - 1; i++) {
+      if (numbers[i] % 2 === 0 && numbers[i + 1] % 2 === 0 && numbers[i + 2] % 2 === 0) {
+        value = 1;
+        break;
+      };
+      else if (numbers[i] % 2 !== 0 && numbers[i + 1] % 2 !== 0 && numbers[i + 2] % 2 !== 0) {
+        value = 1;
+        break;
+      };
+    };
+    if (value === 1) {
        return true;
-     }
-     else {
+    };
+    else {
        return false;
-     }
-   }
- }
+    };
+  };
+ };
 
 function balance(numbers) {
   if (numbers == undefined || numbers.length < 2 || numbers.some(Number.isInteger) === false || numbers.some(isNaN)) {
     return false;
-  }
+  };
   else {
     let values = 0
     for (let i = 0; i < numbers.length; i++) {
-    split = i;
-    secondSplit = i + 1;
-    let sum = 0;
-    let secondSum = 0;
-    for (let j = 0; j <= split; j++) {
-      sum = sum + numbers[j];
-
-    }
-    for (let k = i + 1; k <= numbers.length - 1; k++) {
-      secondSum = secondSum + numbers[k];
-    }
-    if (sum === secondSum) {
-      values = 1;
-      return true;
-    }
-  }
-  if (values !== 1) {
+      split = i;
+      secondSplit = i + 1;
+      let sum = 0;
+      let secondSum = 0;
+      for (let j = 0; j <= split; j++) {
+        sum = sum + numbers[j];
+      };
+      for (let k = i + 1; k <= numbers.length - 1; k++) {
+        secondSum = secondSum + numbers[k];
+      };
+      if (sum === secondSum) {
+        values = 1;
+        return true;
+      };
+    };
+    if (values !== 1) {
     return false;
-  }
-}
+    };
+  };
   console.log("test");
-}
+};
 
 function clumps(values) {
   let clump = 0;
   if (!values) {
     return -1;
-  }
+  };
   else {
      for (let i = 0; i < values.length - 1; i++) {
-      if (values[i - 1] === values[i]) {
-
-      }
-      else if (values[i] === values[i + 1]) {
-        clump++;
-      }
-    }
+       if (values[i - 1] === values[i]) {
+      };
+       else if (values[i] === values[i + 1]) {
+         clump++;
+      };
+     };
     return clump;
-  }
-}
+  };
+};
 
 /*
  * Exports all functions for use in external grader.js file. Do not modify.
