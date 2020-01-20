e15c65b9579075602e2f2b3df991923811f480e0
Guest1 <my@email.comgit config --globak username my>
2020-01-20 09:58:32 -0500

    actually fixed exercise 10

--- src/exercises.js
+++ src/exercises.js
@@ -1,41 +1,41 @@
 function commonEnd(a, b) {
   if (!a || a.length === 0 || !b || b.length === 0) {
     return false;
-  }
+  };
   if (a.length < 1 || b.length < 1 ) {
     return false;
-  }
+  };
 
   if ((a[0] === b[0]) || (a.pop() === b.pop())) {
     return true;
-  }
+  };
   else {
     return false;
-  }
+  };
 };
 
 function endsMeet(values, n) {
   let array = [];
   if (!values || values.length < n || !Number.isInteger(n) || n < 0) {
     return array;
-  }
+  };
   else {
     if (n === 0) {
       return array;
-    }
+    };
       for (let i = 0; i < n; i++) {
         if (values[i] !== undefined) {
           array.push(values[i]);
-        }
-      }
+        };
+      };
       for (let i = n; i > 0; i--) {
         if (values[values.length - i] !== undefined) {
           array.push(values[values.length - i]);
-        }
-      }
+        };
+      };
       return array;
-    }
-  }
+    };
+  };
 
 function difference(numbers) {
   if (numbers == undefined) {
